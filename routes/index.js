ec2da384b623c7b02fa591d0210962812a0d28a0
Prerna Kansal <prerna.swpro@gmail.com>
2016-12-06 14:57:41 +0530

    changes for route file

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Angular Application' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Angular Application'});
 });
 
 module.exports = router;
-
-
-
-
-
